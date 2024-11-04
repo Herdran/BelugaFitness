bf92ea39514b59bed50e3762933eb94fa2ca5769
Mastani-ep <eparuch@student.agh.edu.pl>
2024-11-04 02:45:15 +0100

    Adding main screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(project(":opencv"))
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.appcompat)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
